f31919423896fe4df96d0446309618839e5b7506
Kyle Dunbar <5263943+fullstackdev-us@users.noreply.github.com>
2023-10-21 17:54:55 -0700

    Add contact form api

---
+++ api/ContactFormSubmit/index.js
@@ -0,0 +1,44 @@
+import nodeMailer from 'nodemailer';
+
+module.exports = async function (context, req) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env['smtpUsername'],
+          pass: process.env['smtpPassword']
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env['smtpFromAddress'],
+        to: process.env['smtpTargetAddress'],
+        subject: `New PLD Contact Form Submission: ${formData.Name}`,
+        html: `
+            <h1>New Contact Request From ${formData.Name}</h1>
+            <h2>Email:</h2>
+            <p>${formData.Email}</p>
+            <h2>Message:</h2>
+            <p>${formData.Body}</p>
+        `
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            context.log(error);
+        } else {
+            context.log('Email sent: ' + info.response);
+        }
+      });
+
+    // context.log('JavaScript HTTP trigger function processed a request.');
+
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Success"
+    };
+}
