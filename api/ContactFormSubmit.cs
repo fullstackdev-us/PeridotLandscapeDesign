f929052c8fe19ca11f12cd41c81a2b9cb7dbe4af
Kyle Dunbar <5263943+fullstackdev-us@users.noreply.github.com>
2023-10-21 19:23:18 -0700

    Gmail troubleshooting

--- api/ContactFormSubmit.cs
+++ api/ContactFormSubmit.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -46,8 +45,9 @@ namespace PeridotFunctions
             }
 
             try {
-                SmtpClient mySmtpClient = new SmtpClient(config["smtpServer"])
+                SmtpClient client = new SmtpClient
                 {
+                    Host = config["smtpServer"],
                     Port = int.Parse(config["smtpPort"]),
                     EnableSsl = true,
                     UseDefaultCredentials = false,
@@ -73,23 +73,12 @@ namespace PeridotFunctions
                     IsBodyHtml = true
                 };
 
-                mySmtpClient.Send(myMail);
+                client.Send(myMail);
             }
             catch (Exception ex)
             {
                 log.LogError(ex, "Failed to submit contact form.");
             }
-            // log.LogInformation("C# HTTP trigger function processed a request.");
-
-            // string name = req.Query["name"];
-
-            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // name = name ?? data?.name;
-
-            // string responseMessage = string.IsNullOrEmpty(name)
-            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult("OK");
         }
